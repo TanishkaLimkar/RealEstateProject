a03e617b749923fc8ded09604e10601666042468
piyushlavekar <piyushlavekar0905@gmail.com>
2024-03-07 10:18:04 +0530

    For Push Purpose

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ import express from 'express';
 const app = express();
 
 app.listen(3000, ()=> {
-    console.log('Server Running on port 3000!! for pull purpose');
+    console.log('Server Running on port 3000!! for push purpose');
 });
