67455f922df714561cbbd70c435af97021aef203
piyushlavekar <piyushlavekar0905@gmail.com>
2024-03-07 15:34:30 +0530

    Create User Model

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+//MONGO is your DATABASE URL saved in .env file
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MongoDB!");
 }).catch((err)=>{
